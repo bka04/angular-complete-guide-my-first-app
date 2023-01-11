944cd1504095c0940d9ce219ea3b10aace3d40dd
brent-a <brent.k.aronsen@emcins.com>
2023-01-11 09:28:31 -0600

    Continue section 2 - create a custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent] //lists all components which should be known to Angular when it analyzes our index.html file
+  bootstrap: [AppComponent] //lists all components which should be known to Angular when it analyzes our index.html file at the start
 })
 export class AppModule { }
