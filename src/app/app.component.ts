17a844dae836296fda8186c40ec87df0c557ca75
brent-a <brent.k.aronsen@emcins.com>
2023-01-11 10:20:08 -0600

    Continue section 2 - templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root', //html tag that we are creating, used in index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }
+  // `
+  // ]
 })
 export class AppComponent {
 }
